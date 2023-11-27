c0abbc98e41a465c8ff1a7ee57c717bcad2bdc2d
De Oka <115537848+DeOkanee@users.noreply.github.com>
2023-11-27 14:46:47 +0800

    updatepopup

--- assets/js/RAGU.js
+++ assets/js/RAGU.js
@@ -3,7 +3,8 @@ const ragu = document.getElementById("ragu");
 				Swal.fire({
                     icon: "question",
 					text: "Jika yang anda pilih belum anda ketahui, periksa kembali apa yang anda lihat,\n Karena apa yang anda pilih tergantung dari apa yang anda lihat.",
-				});
+                    footer: '<a href="index.html">TENTUKAN PILIHANMU !</a>'
+                });
 				});
 
 
